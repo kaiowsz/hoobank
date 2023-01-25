5d834d0bd060bb68ded95aa10a67234d07735971
Kaio Silveira <kaionpsa@gmail.com>
2023-01-25 06:15:07 -0300

    hero banner and navbar

---
+++ src/components/index.ts
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+    Navbar, Billing, Business, CTA, CardDeal, Clients, Footer, Hero, Stats, Testimonials
+}
